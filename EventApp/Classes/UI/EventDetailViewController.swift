6c1b489ba40d3461dc7a6e5d952c521e2f33f3c4
Christopher Weber <chrisr.weber@gmail.com>
2015-04-24 06:36:11 +0200

    Create project structure and add basic view controllers

---
+++ EventApp/Classes/UI/EventDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EventDetailViewController.swift
+//  EventApp
+//
+//  Created by Christopher weber on 24.04.15.
+//  Copyright (c) 2015 BFH. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
